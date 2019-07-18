4edf881ae44a92b0543b2637a67ac7883cdcd1b0
dhruv chauhan <dc14690@my.bristol.ac.uk>
2019-07-18 20:59:25 +0200

    ADD: homescreen

---
+++ src/components/Home/index.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import 'tachyons'
+
+import './styles.css'
+
+const Home = () => {
+  return (
+    <div
+      className='w-100 vh-100 bg-dark-blue'
+    >
+      <div
+        className='pa3 pa5-ns ttu sans-serif f3 f1-ns washed-red fw7 ntp'
+      >
+        Cognitive Distortions
+      </div>
+      <div
+        className='lh-copy f5 ph5 pv3 code washed-red'
+      >
+        <b>Negative Thought Patterns,</b> or <b>Cognitive Distortions </b> are irrational thought patterns, or biased ways of thinking,
+        which can lead to mental health issues, such as anxiety, or depression, or relationship problems.
+      </div>
+
+      <div
+        className='lh-copy f5 ph5 pv3 code washed-red'
+      >
+        Essentially, this can mean that the way you are thinking about something is different to the reality of what is happening.
+      </div>
+
+      <div
+        className='lh-copy f5 ph5 pv3 code washed-red'
+      >
+        They are important to understand as often they can lead to a negative loop of thoughts, causing a rapid downturn of mood, and
+        can often happen subconsciously, leaving you in a bad mood.
+      </div>
+
+      <div
+        className='lh-copy f5 ph5 pv3 code washed-red'
+      >
+        Cognitive distortions often lie at the core of <b>CBT</b> (Cognitive Behavioural Therapy) based treatments. Understanding these patterns can help in managing and understanding your mood.
+        This page outlines and explores the most common <b>10 distortions. </b>
+      </div>
+    </div>
+
+  )
+}
+
+export default Home
